04f2f1c9d8b1b5162ae0140a02cf5fc114df9694
learn github <sourabh.sahu10033@gmail.com>
2023-10-06 11:16:20 +0530

    Update the program
    Reverse LinkedList Program

--- Leetcode/MajorityElement2.java
+++ Leetcode/MajorityElement2.java
@@ -1,4 +1,4 @@
-/*Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.*/
+import java.util.*;
 
 public class Solution {
     public List<Integer> majorityElement(int[] nums) {
