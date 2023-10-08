672b514ed4a807007d72e48efc470d8bef514b6f
harshd008 <deoreharsh0812@gmail.com>
2023-10-08 17:08:28 +0530

    odd-even-linkedlist

---
+++ Leetcode/odd_even_linkedlist.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        ListNode temp = head;
+        
+        ListNode odd = new ListNode(0);
+        ListNode tempo = odd;
+        
+        ListNode even = new ListNode(0);
+        ListNode tempe = even;
+        
+        while(temp!=null)
+        {
+            tempo.next = temp;
+            temp = temp.next;
+            tempo = tempo.next;
+            
+            tempe.next = temp;
+            if(temp==null) break;
+            temp = temp.next;
+            tempe = tempe.next;
+        }
+        odd = odd.next;
+        even = even.next;
+        tempo.next = even;
+        
+        return odd;
+    }
+}
